19047b2c24255d5ed6accd891cbf0891fad806ea
toondebooser <toondebooser@gmail.com>
2023-05-12 20:27:09 +0200

    Stuck on a infinite loop on data fetching

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,5 @@
-import { useRef, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
-const Counter = ({todos, setTodos}) => {
+const Counter = ({todos}) => {
 let counter = 0
 todos.map((todo)=>( todo.status == true ? counter += 1 : counter += 0 ))
     return(
